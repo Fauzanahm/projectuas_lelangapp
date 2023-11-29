641d6e8a7c47b447c85f17d413775eba0b6516e7
syihabpargol <fauzan.a.s@students.esqbs.ac.id>
2023-11-29 11:23:30 +0700

    okokok

---
+++ android/app/src/main/kotlin/com/example/projectuas_lelangapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projectuas_lelangapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/theme/widget_themes/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Apptheme {
+  Apptheme._();
+}
+
+static ThemeData LightTheme = ThemeData(
+  brightness: Brightness.light,
+  textTheme: TextTheme.LightTextTheme,
+);
